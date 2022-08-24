112d4e964dfae3e1b9c13d1474fb3580e6bfe99e
vani123r <90778412+vani123r@users.noreply.github.com>
2022-08-24 12:53:12 -0500

    c

--- app/src/main/java/com/example/calcapp/MainActivity.java
+++ app/src/main/java/com/example/calcapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void findSum(View view) {
+        EditText num1 = findViewById(R.id.num1);
+        EditText num2 = findViewById(R.id.num2);
 
     }
 
