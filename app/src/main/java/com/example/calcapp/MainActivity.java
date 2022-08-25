7147bb785d65d37d64a7e6ca948152d796c20d34
vani123r <90778412+vani123r@users.noreply.github.com>
2022-08-25 12:55:10 -0500

    4

--- app/src/main/java/com/example/calcapp/MainActivity.java
+++ app/src/main/java/com/example/calcapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void findSum(View view) {
-        EditText num1 = findViewById(R.id.num1);
-        EditText num2 = findViewById(R.id.num2);
+
+        EditText number1ET = findViewById(R.id.num1);
+        EditText number2ET = findViewById(R.id.num2);
+        TextView numberSumTV = findViewById(R.id.answer);
+        if (number1ET.getText().toString().equals("")){
+            numberSumTV.setText("ENTER NUM");
+        }
+       else if (number2ET.getText().toString().equals("")){
+            numberSumTV.setText("ENTER NUM");
+        }
+       else {
+            double num1 = Integer.parseInt(number1ET.getText().toString());
+            double num2 = Integer.parseInt(number2ET.getText().toString());
+            double sum = num1 + num2;
+
+            numberSumTV.setText("" + sum);
+        }
 
     }
+    public void subtract(View view) {
+        EditText number1ET = findViewById(R.id.num1);
+        EditText number2ET = findViewById(R.id.num2);
+        TextView numberSubTV = findViewById(R.id.answer);
+        if (number1ET.getText().toString().equals("")){
+            numberSumTV.setText("ENTER NUM");
+        }
+        else if (number2ET.getText().toString().equals("")){
+            numberSumTV.setText("ENTER NUM");
+        }
+        else {
+            double num1 = Integer.parseInt(number1ET.getText().toString());
+            double num2 = Integer.parseInt(number2ET.getText().toString());
+            double sub = num1 - num2;
+
+            numberSubTV.setText("" + sub);
+        }
+    }
+    public void multiply(View view) {
+        EditText number1ET = findViewById(R.id.num1);
+        EditText number2ET = findViewById(R.id.num2);
+        TextView numberMultTV = findViewById(R.id.answer);
+
+        double num1 = Integer.parseInt(number1ET.getText().toString());
+        double num2 = Integer.parseInt(number2ET.getText().toString());
+        double mult = (num1 * num2);
+
+        numberMultTV.setText("" + mult);
+
+    }
+    public void divide(View view) {
+        EditText number1ET = findViewById(R.id.num1);
+        EditText number2ET = findViewById(R.id.num2);
+        TextView numberDivTV = findViewById(R.id.answer);
+
+        double num1 = Integer.parseInt(number1ET.getText().toString());
+        double num2 = Integer.parseInt(number2ET.getText().toString());
+        double div = (num1/num2);
+
+        numberDivTV.setText("" + div);
+
+    }
+
 
 }
